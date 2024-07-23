db49edd4a1ae86cf0668249aec1f4342de9419a6
Alexandre Insua <92730122+gbtec-ext-alexandreinsua@users.noreply.github.com>
2024-07-23 09:03:06 +0200

    feat: add task repository service

---
+++ src/app/services/task-repository.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ITask } from '../model/task.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskRepositoryService {
+  private TASK_KEY = 'TASKS';
+  private _tasks: ITask[];
+
+  constructor() {
+    this._tasks = JSON.parse(localStorage.getItem(this.TASK_KEY) || '[]');
+  }
+
+  get tasks(): ITask[] {
+    return this._tasks;
+  }
+
+  save(task: ITask) {
+    this._tasks.push(task);
+    localStorage.setItem(this.TASK_KEY, JSON.stringify(this._tasks));
+  }
+}
